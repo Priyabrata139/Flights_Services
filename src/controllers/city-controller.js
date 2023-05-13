0374e548be50866e3c7bda78238f5bc1fb3ae1a6
Priyabrata Maji <priyabratamaji139@gmail.com>
2023-05-13 23:51:13 +0530

    Added Airplane Api and City Api

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,81 @@
+const { StatusCodes } = require('http-status-codes');
+
+const { CityService } = require('../services');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+
+
+/**
+ * POST : /cities 
+ * req-body {name: 'London'}
+ */
+async function createCity(req, res) {
+    try {
+        const city = await CityService.createCity({
+            name: req.body.name
+        });
+        SuccessResponse.data = city;
+        return res
+                .status(StatusCodes.CREATED)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
+
+
+/**
+ * PATCH: /cities 
+ * req-body {name:Kalyani}
+ */
+async function destroyCity(req, res) {
+    try {
+        const city = await CityService.destroyCity(req.params.id);
+        SuccessResponse.data = city;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
+
+
+
+/**
+ * DELETE: /cities 
+ * req-body {}
+ */
+async function updateCity(req, res) {
+    try {
+        const city = await CityService.updateCity(
+        {
+            name: req.body.name
+        },
+        req.params.id
+        
+    );
+        SuccessResponse.data = city;
+        return res
+                .status(StatusCodes.CREATED)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
+module.exports = {
+    createCity,
+    updateCity,
+    destroyCity
+}
