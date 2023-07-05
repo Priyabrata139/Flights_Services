31dd7903b1a9b6a06655f55980bd6cd0ad4302f6
Priyabrata Maji <priyabratamaji139@gmail.com>
2023-07-05 22:29:42 +0530

    Added error handling for exiox request from booking service and format code

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,120 +1,97 @@
-const { StatusCodes } = require('http-status-codes');
-const { FlightService } = require('../services');
-const { SuccessResponse,ErrorResponse } = require('../utils/common');
+const { StatusCodes } = require("http-status-codes");
+const { FlightService } = require("../services");
+const { SuccessResponse, ErrorResponse } = require("../utils/common");
 
-
-async function createFlight(req,res) {
-    try {
-        const Flight = await FlightService.createFlight({
-            flightNumber:req.body.flightNumber,
-            airplaneId:req.body.airplaneId,
-            departureAirportId:req.body.departureAirportId,
-            arrivalAirportId: req.body.arrivalAirportId,
-            arrivalTime: req.body.arrivalTime,
-            departureTime: req.body.departureTime,
-            price: req.body.price,
-            boardingGate: req.body.boardingGate,
-            totalSeats: req.body.totalSeats
-
-        });
-        SuccessResponse.message='Flight created Succesfully';
-        SuccessResponse.data=Flight;
-        return res.status(StatusCodes.CREATED).json(SuccessResponse);
-
-    } catch (error) {
-        ErrorResponse.message='Cannot created the Flight';
-        ErrorResponse.error=error;
-        return res.status(error.StatusCodes).json(ErrorResponse);
-    }
+async function createFlight(req, res) {
+  try {
+    const Flight = await FlightService.createFlight({
+      flightNumber: req.body.flightNumber,
+      airplaneId: req.body.airplaneId,
+      departureAirportId: req.body.departureAirportId,
+      arrivalAirportId: req.body.arrivalAirportId,
+      arrivalTime: req.body.arrivalTime,
+      departureTime: req.body.departureTime,
+      price: req.body.price,
+      boardingGate: req.body.boardingGate,
+      totalSeats: req.body.totalSeats,
+    });
+    SuccessResponse.message = "Flight created Succesfully";
+    SuccessResponse.data = Flight;
+    return res.status(StatusCodes.CREATED).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.message = "Cannot created the Flight";
+    ErrorResponse.error = error;
+    return res.status(error.StatusCodes).json(ErrorResponse);
+  }
 }
 
-async function getAllFlights(req,res) {
-    try {
-        
-        const Flights = await FlightService.getAllFlights(req.query);
-        SuccessResponse.data=Flights;
-        return res
-        .status(StatusCodes.OK)
-        .json(SuccessResponse);
-    } catch (error) {
-        ErrorResponse.error=error;
-        return res
-        .status(error.StatusCodes)
-        .json(SuccessResponse);
-    }
+async function getAllFlights(req, res) {
+  try {
+    const Flights = await FlightService.getAllFlights(req.query);
+    SuccessResponse.data = Flights;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.StatusCodes).json(SuccessResponse);
+  }
 }
 
 async function updateFlight(req, res) {
-    try {
-      
-        const flight = await FlightService.updateFlight(
-            {
-                totalSeats:req.body.totalSeats
-            },
-            req.params.id
-        );
-        SuccessResponse.data = flight;
-        return res
-                .status(StatusCodes.OK)
-                .json(SuccessResponse);
-    } catch(error) {
-        
-        ErrorResponse.error = error;
-        return res
-                .status(error.statusCode)
-                .json(ErrorResponse);
-    }
+  try {
+    const flight = await FlightService.updateFlight(
+      {
+        totalSeats: req.body.totalSeats,
+      },
+      req.params.id
+    );
+    SuccessResponse.data = flight;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
 }
 
 /**
- * GET : /flights/:id 
+ * GET : /flights/:id
  * req-body {}
  */
 async function getFlight(req, res) {
-    try {
-        const flight = await FlightService.getFlight(req.params.id);
-        SuccessResponse.data = flight;
-        return res
-                .status(StatusCodes.OK)
-                .json(SuccessResponse);
-    } catch(error) {
-        ErrorResponse.error = error;
-        return res
-                .status(error.statusCode)
-                .json(ErrorResponse);
-    }
+  try {
+    const flight = await FlightService.getFlight(req.params.id);
+    SuccessResponse.data = flight;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    console.log("error=");
+    console.log(error);
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
 }
 
-
-
 /**
- * PATCH : /flights/:id/seats 
+ * PATCH : /flights/:id/seats
  * req-body {seats: 3, dec: true}
  */
 async function updateSeats(req, res) {
-    try {
-        console.log(req.body);
-        const response = await FlightService.updateSeats({
-            flightId: req.params.id,
-            seats: req.body.seats, 
-            dec: req.body.dec
-        });
-        SuccessResponse.data = response;
-        return res
-                .status(StatusCodes.OK)
-                .json(SuccessResponse);
-    } catch(error) {
-        ErrorResponse.error = error;
-        return res
-                .status(error.statusCode)
-                .json(ErrorResponse);
-    }
+  try {
+    console.log(req.body);
+    const response = await FlightService.updateSeats({
+      flightId: req.params.id,
+      seats: req.body.seats,
+      dec: req.body.dec,
+    });
+    SuccessResponse.data = response;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
 }
-module.exports={
-    createFlight,
-    getAllFlights,
-    updateFlight,
-    getFlight,
-    updateSeats
-  
-}
+module.exports = {
+  createFlight,
+  getAllFlights,
+  updateFlight,
+  getFlight,
+  updateSeats,
+};
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,148 +1,164 @@
-const { StatusCodes } = require('http-status-codes');
-const {FlightRepository} = require('../repositories');
+const { StatusCodes } = require("http-status-codes");
+const { FlightRepository } = require("../repositories");
 
-const AppError = require('../utils/errors/app-error');
-const { Op } = require('sequelize');
-const db = require('../models');
+const AppError = require("../utils/errors/app-error");
+const { Op } = require("sequelize");
+const db = require("../models");
 console.log(FlightRepository);
 const flightRepository = new FlightRepository();
 
-
 async function createFlight(data) {
-    try {
-        const response = await flightRepository.create(data);
-        return response;
-    } catch (error) {
-        if (error.name == 'SequelizeUniqueConstraintError') {
-            let explantion=[];
-            error.errors.forEach(err => {
-                explantion.push(err.message);
-            });
-        throw new AppError(explantion,StatusCodes.BAD_REQUEST);
-        }
-        console.log(error);
-        throw new AppError(['Something went wrong while creating the fligjt'],StatusCodes.INTERNAL_SERVER_ERROR);
+  try {
+    const response = await flightRepository.create(data);
+    return response;
+  } catch (error) {
+    if (error.name == "SequelizeUniqueConstraintError") {
+      let explantion = [];
+      error.errors.forEach((err) => {
+        explantion.push(err.message);
+      });
+      throw new AppError(explantion, StatusCodes.BAD_REQUEST);
     }
+    console.log(error);
+    throw new AppError(
+      ["Something went wrong while creating the fligjt"],
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
 }
-
 
 async function getAllFlights(query) {
-    try {
-    
-    let customFilter={};
+  try {
+    let customFilter = {};
     let sortFilter;
-    if(query.trips){
-        let departureAirportId,arrivalAirportId;
-        [departureAirportId,arrivalAirportId] = query.trips.split('-');
-        
-      
-        customFilter.departureAirportId=departureAirportId;
-        customFilter.arrivalAirportId=arrivalAirportId;
-    
+    if (query.trips) {
+      let departureAirportId, arrivalAirportId;
+      [departureAirportId, arrivalAirportId] = query.trips.split("-");
+
+      customFilter.departureAirportId = departureAirportId;
+      customFilter.arrivalAirportId = arrivalAirportId;
     }
     if (query.price) {
-        let startingPrice,endingPrice;
-        let defaultEndingPrice;
+      let startingPrice, endingPrice;
+      let defaultEndingPrice;
 
-        [startingPrice,endingPrice] = query.price.split('-');
+      [startingPrice, endingPrice] = query.price.split("-");
 
-        defaultEndingPrice= parseInt(startingPrice) +1000;
-        customFilter.price = {
-            [Op.between] : [startingPrice,endingPrice==undefined?defaultEndingPrice:endingPrice]
-        }
+      defaultEndingPrice = parseInt(startingPrice) + 1000;
+      customFilter.price = {
+        [Op.between]: [
+          startingPrice,
+          endingPrice == undefined ? defaultEndingPrice : endingPrice,
+        ],
+      };
     }
     if (query.travellers) {
-        customFilter.totalSeats = {
-            [Op.gte] : query.travellers
-        }
+      customFilter.totalSeats = {
+        [Op.gte]: query.travellers,
+      };
     }
     if (query.tripDate) {
-        let startingDepartureTime, endingDepartureTime, defaultTime;
-        
+      let startingDepartureTime, endingDepartureTime, defaultTime;
 
-        startingDepartureTime = query.tripDate;
-        [endingDepartureTime, defaultTime] = startingDepartureTime.split('T');
-        defaultTime = 'T23:59:59.000Z';
-        endingDepartureTime=endingDepartureTime+defaultTime;
-        customFilter.departureTime = {
-            [Op.between] : [startingDepartureTime, endingDepartureTime]
-        }
-        console.log(endingDepartureTime);
+      startingDepartureTime = query.tripDate;
+      [endingDepartureTime, defaultTime] = startingDepartureTime.split("T");
+      defaultTime = "T23:59:59.000Z";
+      endingDepartureTime = endingDepartureTime + defaultTime;
+      customFilter.departureTime = {
+        [Op.between]: [startingDepartureTime, endingDepartureTime],
+      };
+      console.log(endingDepartureTime);
     }
-
 
     if (query.sort) {
-        let params = query.sort.split(',');
-        console.log(params);
-        sortFilter = params.map((val)=>val.split('_'));
+      let params = query.sort.split(",");
+      console.log(params);
+      sortFilter = params.map((val) => val.split("_"));
     }
-    
-    const Flights= await flightRepository.getAllFlights(customFilter,sortFilter);
+
+    const Flights = await flightRepository.getAllFlights(
+      customFilter,
+      sortFilter
+    );
     return Flights;
-    } catch (error) {
-        console.log(error);
-        throw new AppError(['Something went wrong while creating the fligjt'],StatusCodes.INTERNAL_SERVER_ERROR);
-    }
-
+  } catch (error) {
+    console.log(error);
+    throw new AppError(
+      ["Something went wrong while creating the fligjt"],
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
 }
-
 
 async function getFlight(id) {
-    try {
-        const flight = await flightRepository.get(id);
-        return flight;
-    } catch(error) {
-        if(error.statusCode == StatusCodes.NOT_FOUND) {
-            throw new AppError('The flight you requested is not present', error.statusCode);
-        }
-        throw new AppError('Cannot fetch data of all the flights', StatusCodes.INTERNAL_SERVER_ERROR);
+  try {
+    const flight = await flightRepository.get(id);
+    return flight;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        ["The flight you requested is not present"],
+        error.statusCode
+      );
     }
+    throw new AppError(
+      "Cannot fetch data of all the flights",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
 }
 
-async function updateFlight(data,id) {
-    try {
-       
-        const flight = await flightRepository.update(data,id);
-        return flight;
-    } catch(error) { 
-        console.log(error);
-        if(error.statusCode == StatusCodes.NOT_FOUND) {
-            throw new AppError('The flight you requested to update is not present', error.statusCode);
-        }
-        else if(error.name == 'SequelizeValidationError') {
-            let explanation = [];
-            error.errors.forEach((err) => {
-                explanation.push(err.message);
-            });
-            throw new AppError(explanation, StatusCodes.BAD_REQUEST);
-        }
-        throw new AppError('Cannot fetch data of all the flights', StatusCodes.INTERNAL_SERVER_ERROR);
+async function updateFlight(data, id) {
+  try {
+    const flight = await flightRepository.update(data, id);
+    return flight;
+  } catch (error) {
+    console.log(error);
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The flight you requested to update is not present",
+        error.statusCode
+      );
+    } else if (error.name == "SequelizeValidationError") {
+      let explanation = [];
+      error.errors.forEach((err) => {
+        explanation.push(err.message);
+      });
+      throw new AppError(explanation, StatusCodes.BAD_REQUEST);
     }
+    throw new AppError(
+      "Cannot fetch data of all the flights",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
 }
-
-
-
-
 
 async function updateSeats(data) {
-    
-    try {
-        const response = await flightRepository.updateRemainingSeats(data.flightId, data.seats, data.dec);
-        return response;
-    } catch(error) {
-       
-        if(error.statusCode == StatusCodes.NOT_FOUND) {
-            throw new AppError('The flight you requested to update is not present', error.statusCode);
-        }
-        throw new AppError('Cannot update data of the flight', StatusCodes.INTERNAL_SERVER_ERROR);
+  try {
+    const response = await flightRepository.updateRemainingSeats(
+      data.flightId,
+      data.seats,
+      data.dec
+    );
+    return response;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The flight you requested to update is not present",
+        error.statusCode
+      );
     }
+    throw new AppError(
+      "Cannot update data of the flight",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
 }
 
-module.exports={
-    createFlight,
-    getAllFlights,
-    getFlight,
-    updateFlight,
-    updateSeats
-    
-}
+module.exports = {
+  createFlight,
+  getAllFlights,
+  getFlight,
+  updateFlight,
+  updateSeats,
+};
